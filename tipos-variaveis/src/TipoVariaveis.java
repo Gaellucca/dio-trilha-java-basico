22787dc07ac9449dfee7c0e365409234d7e2ecc3
Gaellucca <jefersonflutter@outlook.com>
2024-06-11 14:17:41 -0300

    tipos-primitivos

---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,22 @@
+public class TipoVariaveis {
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudo de classe String que representao texto na aplicaçao
+        
+        String meuNome = "Jeferson Andrade";
+
+        Double salarioMinimo = 2.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 =(short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+
+        System.out.println(numero);
+    }
+}
+ 
